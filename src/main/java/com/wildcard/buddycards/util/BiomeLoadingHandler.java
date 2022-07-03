16d45d63021571657cd53ab29e362b24f114e363
wildcardgamez <75537188+wildcardgamez@users.noreply.github.com>
2022-07-03 12:10:38 -0400

    Quick push before changing file location

--- src/main/java/com/wildcard/buddycards/util/BiomeLoadingHandler.java
+++ src/main/java/com/wildcard/buddycards/util/BiomeLoadingHandler.java
@@ -1,11 +1,18 @@
 package com.wildcard.buddycards.util;
 
+import com.wildcard.buddycards.registries.BuddycardsFeatures;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class BiomeLoadingHandler {
     @SubscribeEvent
     public void onBiomeLoad(BiomeLoadingEvent event) {
-
+        if(BiomeDictionary.getTypes(ResourceKey.create(Registry.BIOME_REGISTRY, event.getName())).contains(BiomeDictionary.Type.HOT)) {
+            event.getGeneration().getFeatures(GenerationStep.Decoration.RAW_GENERATION).add(BuddycardsFeatures.PLACED_LUMINIS_VEIN.getHolder().get());
+        }
     }
 }
--- src/main/java/com/wildcard/buddycards/worldgen/LuminisVeinFeature.java
+++ src/main/java/com/wildcard/buddycards/worldgen/LuminisVeinFeature.java
@@ -1,9 +1,15 @@
 package com.wildcard.buddycards.worldgen;
 
 import com.mojang.serialization.Codec;
+import com.wildcard.buddycards.registries.BuddycardsBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+
+import java.util.Random;
 
 public class LuminisVeinFeature extends Feature<NoneFeatureConfiguration> {
     public LuminisVeinFeature(Codec<NoneFeatureConfiguration> codec) {
@@ -12,6 +18,40 @@ public class LuminisVeinFeature extends Feature<NoneFeatureConfiguration> {
 
     @Override
     public boolean place(FeaturePlaceContext context) {
-        return false;
+        BlockPos blockpos = context.origin();
+        WorldGenLevel worldgenlevel = context.level();
+        Random random = context.random();
+
+        for(; blockpos.getY() > worldgenlevel.getMinBuildHeight() + 3; blockpos = blockpos.below()) {
+            if (!worldgenlevel.isEmptyBlock(blockpos.below())) {
+                BlockState blockstate = worldgenlevel.getBlockState(blockpos.below());
+                if (isDirt(blockstate) || isStone(blockstate)) {
+                    break;
+                }
+            }
+        }
+
+        if (blockpos.getY() <= worldgenlevel.getMinBuildHeight() + 10) {
+            return false;
+        } else {
+            for(int l = 0; l < 7; ++l) {
+                int i = random.nextInt(8);
+                int j = random.nextInt(8);
+                int k = random.nextInt(8);
+                float f = (float)(i + j + k) * 0.333F + 0.5F;
+
+                for(BlockPos blockpos1 : BlockPos.betweenClosed(blockpos.offset(-i, -j, -k), blockpos.offset(i, j, k))) {
+                    if (blockpos1.distSqr(blockpos) <= (double)(f * f)) {
+                        if(blockpos1.getY() > 0)
+                            worldgenlevel.setBlock(blockpos1, BuddycardsBlocks.LUMINIS_ORE.get().defaultBlockState(), 4);
+                        else
+                            worldgenlevel.setBlock(blockpos1, BuddycardsBlocks.DEEPSLATE_LUMINIS_ORE.get().defaultBlockState(), 4);
+                    }
+                }
+
+                blockpos = blockpos.offset(-1 + random.nextInt(2), -random.nextInt(2), -1 + random.nextInt(2));
+            }
+            return true;
+        }
     }
 }
