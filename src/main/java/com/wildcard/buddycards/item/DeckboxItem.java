7325b7914fab9469b88ca51bf0f4b686d43ac7e9
wildcardgamez <75537188+wildcardgamez@users.noreply.github.com>
2022-12-12 12:55:52 -0500

    GAHHHH also luminis crystal box recipe

--- src/main/java/com/wildcard/buddycards/item/DeckboxItem.java
+++ src/main/java/com/wildcard/buddycards/item/DeckboxItem.java
@@ -1,18 +1,11 @@
 package com.wildcard.buddycards.item;
 
-import com.wildcard.buddycards.menu.DeckboxMenu;
 import com.wildcard.buddycards.inventory.DeckboxInventory;
-<<<<<<< Updated upstream
-import net.minecraft.core.NonNullList;
-=======
->>>>>>> Stashed changes
+import com.wildcard.buddycards.menu.DeckboxMenu;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
-<<<<<<< Updated upstream
-=======
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
->>>>>>> Stashed changes
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -21,15 +14,11 @@ import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.network.NetworkHooks;
 
-<<<<<<< Updated upstream
-import java.util.Optional;
-import java.util.stream.Stream;
-=======
 import java.util.List;
->>>>>>> Stashed changes
 
 public class DeckboxItem extends Item {
     public DeckboxItem(Properties properties) {
@@ -39,7 +28,7 @@ public class DeckboxItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack deck = player.getItemInHand(hand);
-        if(level instanceof ServerLevel) {
+        if (level instanceof ServerLevel) {
             NetworkHooks.openGui((ServerPlayer) player, new SimpleMenuProvider(
                     (id, playerInventory, entity) -> new DeckboxMenu(id, player.getInventory(), new DeckboxInventory(deck))
                     , player.getItemInHand(hand).getHoverName()));
@@ -47,14 +36,18 @@ public class DeckboxItem extends Item {
         return InteractionResultHolder.success(player.getItemInHand(hand));
     }
 
-    public Optional<TooltipComponent> getTooltipImage(ItemStack stack) {
-        NonNullList<ItemStack> nonnulllist = NonNullList.create();
-        if(stack.hasTag() && stack.getTag().contains("Items")) {
-            for (Tag tag : stack.getTag().getList("Items", Tag.TAG_COMPOUND)) {
-                if(tag instanceof CompoundTag nbt)
-                    nonnulllist.add(nbt.getInt("Slot"), ItemStack.of(nbt));
-            }
+    @Override
+    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
+        if (stack.hasTag() && stack.getTag().contains("Deck")) {
+            CompoundTag deck = stack.getTag().getCompound("Deck");
+            for (String i : deck.getAllKeys())
+                tooltip.add(new TextComponent(deck.get(i) + " " + i));
         }
-        return Optional.of(new BundleTooltip(nonnulllist, nonnulllist.size()));
+    }
+
+    public boolean isFull(ItemStack stack) {
+        if(stack.hasTag() && stack.getTag().contains("Items"))
+            return stack.getTag().getList("Items", Tag.TAG_COMPOUND).size() == 16;
+        return false;
     }
 }
