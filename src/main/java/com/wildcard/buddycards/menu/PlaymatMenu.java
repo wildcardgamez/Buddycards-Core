0a132a456a0100cc916aa75328ccc2521fcf864d
wildcardgamez <75537188+wildcardgamez@users.noreply.github.com>
2023-02-09 22:48:25 -0500

    Variable name I guess?

--- src/main/java/com/wildcard/buddycards/menu/PlaymatMenu.java
+++ src/main/java/com/wildcard/buddycards/menu/PlaymatMenu.java
@@ -4,7 +4,6 @@ import com.wildcard.buddycards.Buddycards;
 import com.wildcard.buddycards.container.BattleContainer;
 import com.wildcard.buddycards.registries.BuddycardsMisc;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
@@ -16,7 +15,7 @@ import net.minecraft.world.item.ItemStack;
 import java.util.ArrayList;
 
 public class PlaymatMenu extends AbstractContainerMenu {
-    BattleContainer playerContainer;
+    BattleContainer container;
 
     public PlaymatMenu(int id, Inventory playerInv) {
         this(id, playerInv, new BattleContainer(), true);
@@ -24,17 +23,17 @@ public class PlaymatMenu extends AbstractContainerMenu {
 
     public PlaymatMenu(int id, Inventory playerInv, BattleContainer container, boolean p1) {
         super(BuddycardsMisc.PLAYMAT_CONTAINER.get(), id);
-        this.playerContainer = container;
-        this.addSlot(new DeckSlot(this.playerContainer, (p1 ? 7 : 0), 143, 18));
-        this.addSlot(new DeckSlot(this.playerContainer, (p1 ? 0 : 7), 143, 64));
+        this.container = container;
+        this.addSlot(new DeckSlot(this.container, (p1 ? 7 : 0), 143, 18));
+        this.addSlot(new DeckSlot(this.container, (p1 ? 0 : 7), 143, 64));
         for (int i = 0; i < 3; i++) {
-            this.addSlot(new CardSlot(this.playerContainer, (p1 ? 1 : 8)+i, 80 + (18 * i), 64));
+            this.addSlot(new CardSlot(this.container, (p1 ? 1 : 8) + i, 80 + (18 * i), 64));
         }
         for (int i = 0; i < 3; i++) {
-            this.addSlot(new BattlefieldSlot(this.playerContainer, (p1 ? 4 : 11)+i, 80 + (18 * i), 36));
+            this.addSlot(new BattlefieldSlot(this.container, (p1 ? 4 : 11)+i, 80 + (18 * i), 36));
         }
         for (int i = 0; i < 3; i++) {
-            this.addSlot(new OpponentBattlefieldSlot(this.playerContainer, (p1 ? 11 : 4)+i, 80 + (18 * i), 18));
+            this.addSlot(new OpponentBattlefieldSlot(this.container, (p1 ? 11 : 4)+i, 80 + (18 * i), 18));
         }
     }
 
@@ -92,7 +91,7 @@ public class PlaymatMenu extends AbstractContainerMenu {
     }
 
     public MutableComponent getBattleLog() {
-        ArrayList<MutableComponent> battleLog = playerContainer.battleLog;
+        ArrayList<MutableComponent> battleLog = container.battleLog;
         if(battleLog.isEmpty())
             return new TranslatableComponent(Buddycards.MOD_ID + ".broken");
         MutableComponent component = battleLog.get(battleLog.size());
